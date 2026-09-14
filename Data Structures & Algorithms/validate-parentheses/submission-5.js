95da49be48b2631800aec100ea3bd22b9dee2a6c
TakajoX <gabrieletomasino.x@gmail.com>
2026-09-14 10:56:21 +0200

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+
+        const stack = [];
+
+        for(let i = 0; i < s.length; i++) {
+
+            if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+                stack.push(s[i]);
+            } else if (s[i] == ")" && stack.pop() != "(") {
+                return false;
+            }
+            else if (s[i] == "]" && stack.pop() != "[") {
+                return false;
+            }
+            else if (s[i] == "}" && stack.pop() != "{") {
+                return false;
+            }
+        }
+
+        if (stack.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
